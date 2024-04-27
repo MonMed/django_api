057efd65fdad5de48719d8c5965f9a2322d86752
MonMed <monicameduri1@gmail.com>
2024-04-26 17:10:09 -0700

    initial commit

---
+++ courses/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    tutor = models.CharField(max_length =100)
+    description = models.CharField(max_length=100)
---
+++ courses/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Course
+from .serializers import CourseSerializer 
+from rest_framework import viewsets
+
+
+class CourseView(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+     
+
