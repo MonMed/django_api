1a0f0c236fb2dbc1041d4b530d7530fe6d0ff47b
MonMed <monicameduri1@gmail.com>
2024-04-26 17:23:57 -0700

    updated model

--- courses/models.py
+++ courses/models.py
@@ -4,3 +4,6 @@ class Course(models.Model):
     name = models.CharField(max_length=100)
     tutor = models.CharField(max_length =100)
     description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
